a8877ae2d3dfb0ee2f83b396d02e0c7283b2a123
Matthew Panszczyk <distinctivebuilders@hotmail.com>
2025-12-05 10:50:08 -0500

    Import useEffect for contact form logic

--- src/Site.tsx
+++ src/Site.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import { Pencil, Hammer, Home } from "lucide-react";
 import { Link } from "react-router-dom";
